bd21749a7bffa126a4a1589593ad506699e27936
Ana Clara Mendes <anamendes0813@gmail.com>
2025-09-21 17:19:28 -0300

    chore: WIP antes do pull

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,29 +2,21 @@
 // Convenção de mercado: colocar em cmd/<nome-app>/main.go
 package main
 
-// Importa os pacotes necessários
+import (
+	// "fmt"
+	// "github.com/AnaClara0813/meu-projeto-go/internal/fibonacci"
+	// "github.com/AnaClara0813/meu-projeto-go/internal/hello"
 
-//"fmt"
+	"github.com/AnaClara0813/meu-projeto-go/internal/memoria-go"
+)
 
-//"github.com/AnaClara0813/meu-projeto-go/internal/fibonacci"
-//"github.com/AnaClara0813/meu-projeto-go/internal/hello"
-//"github.com/AnaClara0813/meu-projeto-go/internal/memoria-go"
-
-// Função principal do programa
 func main() {
+	// fmt.Println("🚀 Meu primeiro projeto em Go com estrutura de mercado!")
+	// hello.SayHello()
+	// n := 8
+	// valor := fibonacci.Fibonacci(n)
+	// fmt.Printf("F(%d) = %d\n", n, valor)
+	// fibonacci.PrintSequence(n)
 
-	//fmt.Println("🚀 Meu primeiro projeto em Go com estrutura de mercado!")
-
-	//hello.SayHello()
-
-	//n := 8
-
-	//valor := fibonacci.Fibonacci(n)
-
-	//fmt.Printf("F(%d) = %d\n", n, valor)
-
-	//fibonacci.PrintSequence(n)
-
-	 memoriago.Play()
-
+	memoriago.Play()
 }
