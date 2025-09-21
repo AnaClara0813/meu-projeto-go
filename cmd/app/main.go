3e3d0d26f6a3cded52d4eeedb2296f2629cd2ddd
Ana Clara Mendes <anamendes0813@gmail.com>
2025-09-21 17:09:40 -0300

    feat: ajustes no main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,29 +3,28 @@
 package main
 
 // Importa os pacotes necessários
-import (
-	//"fmt"
 
-	//"github.com/AnaClara0813/meu-projeto-go/internal/fibonacci"
-	//"github.com/AnaClara0813/meu-projeto-go/internal/hello"
-	"github.com/AnaClara0813/meu-projeto-go/internal/memoria-go"
-)
+//"fmt"
+
+//"github.com/AnaClara0813/meu-projeto-go/internal/fibonacci"
+//"github.com/AnaClara0813/meu-projeto-go/internal/hello"
+//"github.com/AnaClara0813/meu-projeto-go/internal/memoria-go"
 
 // Função principal do programa
 func main() {
-	
+
 	//fmt.Println("🚀 Meu primeiro projeto em Go com estrutura de mercado!")
 
 	//hello.SayHello()
 
 	//n := 8
-	
+
 	//valor := fibonacci.Fibonacci(n)
 
 	//fmt.Printf("F(%d) = %d\n", n, valor)
 
 	//fibonacci.PrintSequence(n)
 
-    memoriago.Play()
+	 memoriago.Play()
 
 }
